8002d3c4a5fe2d03e79de9c3e926622088211b57
Hari <hari@testpress.in>
2024-06-05 15:15:29 +0530

    feat: Setup shaka player with offline support (#1)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const { app, BrowserWindow } = require('electron');
 const path = require('node:path');
+const { app, components, BrowserWindow } = require('electron');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
+
 
 const createWindow = () => {
   // Create the browser window.
@@ -13,6 +14,8 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
     },
   });
 
@@ -26,9 +29,11 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
+app.whenReady().then(async () => {
+  await components.whenReady();
+  console.log('components ready:', components.status());
   createWindow();
-
+  
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   app.on('activate', () => {
